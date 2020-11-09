b5eb17ece76c607f17ebc4657bb08092bc70c255
AuroreFournier <aurore.fournier0809@gmail.com>
2020-11-09 15:28:19 +0100

    ajout poeme aurore

--- contact.php
+++ contact.php
@@ -2,4 +2,8 @@
 
 
 echo'eofn,ifsw';
+echo'Le Hibou, de retour, ne trouve que les pieds
+De ses chers Nourrissons, hélas ! pour toute chose.
+Il se plaint; et les dieux sont par lui suppliés';
+
  ?>
